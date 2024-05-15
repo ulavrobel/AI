6fb35848c3bdce1120f7edb12ccea5634e8652b9
ulavrobel <ulawrobel@o2.pl>
2024-05-15 21:00:12 +0200

    First commit

--- decision_tree.py
+++ decision_tree.py
@@ -1,17 +1,7 @@
+from lib2to3.pytree import Node
+
 import numpy as np
 from collections import Counter
-
-
-class Node:
-    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
-        self.feature = feature
-        self.threshold = threshold
-        self.left = left
-        self.right = right
-        self.value = value
-
-    def is_leaf_node(self):
-        return self.value is not None
 
 
 class DecisionTree:
@@ -22,30 +12,80 @@ class DecisionTree:
         self.root = None
 
     def fit(self, X, y):
-        pass
+        self.n_features = X.shape[1] if not self.n_features else min(self.n_features, X.shape[1])
+        self.root = self._grow_tree(X, y)
 
     def _grow_tree(self, X, y, depth=0):
-        pass
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
+
+        # Stop conditions
+        if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_features, self.n_features, replace=False)
+
+        best_feat, best_threshold = self._best_split(X, y, feat_idxs)
+
+        if best_feat is None:
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        left_idxs, right_idxs = self._split(X[:, best_feat], best_threshold)
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth + 1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth + 1)
+        return Node(feature=best_feat, threshold=best_threshold, left=left, right=right)
 
     def _best_split(self, X, y, feat_idxs):
-        pass
+        best_gain = -1
+        best_feat, best_threshold = None, None
+        for feat_idx in feat_idxs:
+            X_column = X[:, feat_idx]
+            thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                gain = self._information_gain(y, X_column, threshold)
+                if gain > best_gain:
+                    best_gain = gain
+                    best_feat = feat_idx
+                    best_threshold = threshold
+        return best_feat, best_threshold
 
     def _information_gain(self, y, X_column, threshold):
-        return
+        parent_entropy = self._entropy(y)
+        left_idxs, right_idxs = self._split(X_column, threshold)
+
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+
+        left_entropy = self._entropy(y[left_idxs])
+        right_entropy = self._entropy(y[right_idxs])
+
+        child_entropy = (len(left_idxs) / len(y)) * left_entropy + (len(right_idxs) / len(y)) * right_entropy
+        return parent_entropy - child_entropy
 
     def _split(self, X_column, split_threshold):
-        pass
+        left_idxs = np.where(X_column <= split_threshold)[0]
+        right_idxs = np.where(X_column > split_threshold)[0]
+        return left_idxs, right_idxs
 
     def _entropy(self, y):
         hist = np.bincount(y)
         ps = hist / len(y)
-        return - np.sum([p * np.log(p) for p in ps if p > 0])
+        return -np.sum([p * np.log2(p) for p in ps if p > 0])
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
 
     def _traverse_tree(self, x, node):
-        pass
+        if node.is_leaf_node():
+            return node.value
+
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
 
     def predict(self, X):
         return np.array([self._traverse_tree(x, self.root) for x in X])
-
-
-
