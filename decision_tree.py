98c02dc3306debcf48a362e1d7c5632767a86ed3
ulavrobel <ulawrobel@o2.pl>
2024-05-15 21:12:11 +0200

    Commit

--- decision_tree.py
+++ decision_tree.py
@@ -1,9 +1,16 @@
-from lib2to3.pytree import Node
-
 import numpy as np
 from collections import Counter
 
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
 
+    def is_leaf_node(self):
+        return self.value is not None
 class DecisionTree:
     def __init__(self, min_samples_split=2, max_depth=100, n_features=None):
         self.min_samples_split = min_samples_split
@@ -19,18 +26,14 @@ class DecisionTree:
         n_samples, n_features = X.shape
         n_labels = len(np.unique(y))
 
-        # Stop conditions
         if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split):
-            leaf_value = self._most_common_label(y)
+            counter = Counter(y)
+            leaf_value = counter.most_common(1)[0][0]
             return Node(value=leaf_value)
 
         feat_idxs = np.random.choice(n_features, self.n_features, replace=False)
 
         best_feat, best_threshold = self._best_split(X, y, feat_idxs)
-
-        if best_feat is None:
-            leaf_value = self._most_common_label(y)
-            return Node(value=leaf_value)
 
         left_idxs, right_idxs = self._split(X[:, best_feat], best_threshold)
         left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth + 1)
@@ -74,10 +77,6 @@ class DecisionTree:
         ps = hist / len(y)
         return -np.sum([p * np.log2(p) for p in ps if p > 0])
 
-    def _most_common_label(self, y):
-        counter = Counter(y)
-        most_common = counter.most_common(1)[0][0]
-        return most_common
 
     def _traverse_tree(self, x, node):
         if node.is_leaf_node():
